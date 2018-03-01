1321c818582f77df2f65f6e4a05310bc3b8933bd
debbie <drosenfeld87@gmail.com>
2018-03-01 17:15:03 -0500

    assignment complete

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
     root 'pictures#index'
     resources :pictures
     resources :users, only: [:new, :create]
+    resources :sessions, only: [:new, :create, :destroy]
   end
