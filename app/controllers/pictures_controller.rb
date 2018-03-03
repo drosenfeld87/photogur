2ce2f3c19ccf2f429627113d6a1e48d7463feade
debbie <drosenfeld87@gmail.com>
2018-03-03 11:49:28 -0500

    authorizing picture owner works

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,23 +1,52 @@
 class PicturesController < ApplicationController
 
   before_action :ensure_logged_in, except: [:show, :index]
+  before_action :load_picture, only: [:show, :edit, :update, :destroy]
 
   def index
     @pictures = Picture.all
     @most_recent_pictures = Picture.created_before(Date.today - 1.month)
   end
 
+  def update
+    @picture.title = params[:picture][:title]
+    @picture.artist = params[:picture][:artist]
+    @picture.url = params[:picture][:url]
+
+    if @picture.save
+      redirect_to "/pictures/#{@picture.id}"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @picture.destroy
+    redirect_to "/pictures"
+  end
+
+  def load_picture
+    @picture = Picture.find(params[:id])
+  end
+
+    def ensure_user_owns_picture
+    unless current_user == @picture.user
+      flash[:alert] = "Please log in"
+      redirect_to new_session_url
+    end
+  end
+
   def show
-   @picture = Picture.find(params[:id])
+   # @picture = Picture.find(params[:id])
   end
 
   def new
     @picture = Picture.new
   end
 
-  def edit
-    @picture = Picture.find(params[:id])
-  end
+  # def edit
+  #   @picture = Picture.find(params[:id])
+  # end
 
   def create
     @picture = Picture.new
@@ -36,7 +65,7 @@ class PicturesController < ApplicationController
   end
 
     def update
-    @picture = Picture.find(params[:id])
+    # @picture = Picture.find(params[:id])
 
     @picture.title = params[:picture][:title]
     @picture.artist = params[:picture][:artist]
